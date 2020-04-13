5aedb32672b7a28e6e34c276d7e45f0c243deaed
Wenhan Wang <wqy19970911@hotmail.com>
2020-04-13 12:53:06 -0400

    Tomcat server deployment

--- src/main/java/com/u1s1/edq/controller/StateController.java
+++ src/main/java/com/u1s1/edq/controller/StateController.java
@@ -1,4 +1,19 @@
 package com.u1s1.edq.controller;
 
-public class StateController  {
+import com.u1s1.edq.entity.DemoData;
+import com.u1s1.edq.entity.StateEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StateController {
+
+    @GetMapping(value = "/state/{stateId}")
+    public StateEntity selectState(@PathVariable String stateId) {
+        // method body here
+
+        return null;
+    }
+
 }
