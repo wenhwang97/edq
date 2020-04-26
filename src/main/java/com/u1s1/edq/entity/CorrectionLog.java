ff8224f04c74fa4b8c99e60b76f3a01b9435bbb8
Wenhan Wang <wqy19970911@hotmail.com>
2020-04-26 02:28:04 -0400

    Add new entity files

---
+++ src/main/java/com/u1s1/edq/entity/CorrectionLog.java
@@ -0,0 +1,52 @@
+package com.u1s1.edq.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Table(name = "CORRECTION_LOG")
+@Entity
+public class CorrectionLog implements Serializable {
+
+    private Integer id;
+
+    private Precinct precinct;
+    private LocalDateTime ModifyTime = LocalDateTime.now();
+    private String Loginfo;
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "precinct_id")
+    public Precinct getPrecinct() {
+        return precinct;
+    }
+
+    public void setPrecinct(Precinct precinct) {
+        this.precinct = precinct;
+    }
+
+    @Column(name = "correction_time")
+    public LocalDateTime getModifyTime(){return ModifyTime;}
+
+    public void setModifyTime(LocalDateTime time){this.ModifyTime=time;}
+
+    @Column(name = "description")
+    public String getLoginfo(){return Loginfo;}
+
+    public void setLoginfo(String loginfo) {
+        Loginfo = loginfo;
+    }
+
+}
---
+++ src/main/java/com/u1s1/edq/entity/Error.java
@@ -0,0 +1,55 @@
+package com.u1s1.edq.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Table(name="ERROR")
+@Entity
+public class Error implements Serializable {
+
+    private Integer id;
+
+    private Precinct precinct;
+    private String ErrorInfo;
+    private String ErrorType;
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @Column(name = "error_id")
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "precinct_id")
+    public Precinct getPrecinct() {
+        return precinct;
+    }
+
+    public void setPrecinct(Precinct precinct) {
+        this.precinct = precinct;
+    }
+
+    public String getErrorInfo() {
+        return ErrorInfo;
+    }
+
+    public void setErrorInfo(String errorInfo) {
+        ErrorInfo = errorInfo;
+    }
+
+    public String getErrorType() {
+        return ErrorType;
+    }
+
+    public void setErrorType(String errorType) {
+        ErrorType = errorType;
+    }
+}
