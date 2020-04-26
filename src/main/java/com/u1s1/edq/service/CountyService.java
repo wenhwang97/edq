9ae2cdea487ca349192c350efb5b46a55aac9504
Wenhan Wang <wqy19970911@hotmail.com>
2020-04-26 02:23:29 -0400

    Remove unnecessary methods

--- src/main/java/com/u1s1/edq/service/CountyService.java
+++ src/main/java/com/u1s1/edq/service/CountyService.java
@@ -2,8 +2,6 @@ package com.u1s1.edq.service;
 
 import com.u1s1.edq.entity.County;
 import com.u1s1.edq.entity.DemoData;
-import com.u1s1.edq.entity.CountyElectionData;
-import com.u1s1.edq.enums.ElectionType;
 import com.u1s1.edq.repository.CountyRepository;
 import com.u1s1.edq.service.cache.CachedContainer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,34 +25,10 @@ public class CountyService {
         return cachedContainer.findCounty(stateId, countyId);
     }
 
-
     public void updateCountyDemoData(String stateId, String countyId, DemoData demoData) {
         County county = cachedContainer.findCounty(stateId, countyId);
         county.setDemoData(demoData);
         countyRepo.save(county);
     }
 
-
-    public CountyElectionData getElectionData(String stateId, String countyId, ElectionType type, int year) {
-
-        County county = cachedContainer.findCounty(stateId, countyId);
-
-        for (CountyElectionData electionData : county.getElectionData()) {
-            if (year == electionData.getYear() && electionData.getType() == type) return electionData;
-        }
-
-        return null;
-    }
-
-    public void updateElectionData(String stateId, String countyId,
-                               ElectionType type, int year, CountyElectionData data) {
-
-        County county = cachedContainer.findCounty(stateId, countyId);
-
-        for (CountyElectionData electionData : county.getElectionData()) {
-            if (year == electionData.getYear() && electionData.getType() == type) electionData = data;
-        }
-
-        countyRepo.save(county);
-    }
 }
