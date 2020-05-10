2570fddf1fdfb31682bd1eb74d0b513d38f2749a
cosecant <38693290+cosecant123@users.noreply.github.com>
2020-05-09 22:47:57 -0400

    modify change boundary

--- src/main/resources/static/js/precinctFunctions.js
+++ src/main/resources/static/js/precinctFunctions.js
@@ -224,19 +224,20 @@ function precinctEvents(stateName,county){  //here shouldn't be county should be
 }
 async function precinctChangeBoundary(url, data) {
     console.log(data);
-    var coord =[];
+    var vertices =[];
     // precinctPolygon.push({lat: precinctJson[i].obj[j].vertices[k].y_pos, lng: precinctJson[i].obj[j].vertices[k].x_pos});
     for(var i in data){
-        coord.push({x_pos: data[i].lng, y_pos: data[i].lat});
+        vertices.push({x_pos: data[i].lng, y_pos: data[i].lat});
     }
-    console.log(coord);
+    console.log(vertices);
+    console.log(JSON.stringify({vertices}));
     $.blockUI({message: '<h1><img src="../images/YCZH.gif" /> Loding Counties</h1>'});
 
-    // console.log(JSON.stringify(data));
-    // console.log(JSON.stringify({data}));
+    console.log(JSON.stringify(data));
+    console.log(JSON.stringify({data}));
     await fetch(url, {
         method: 'PUT', // or 'PUT'
-        body: JSON.stringify({coord}), // data can be `string` or {object}!
+        body: JSON.stringify({vertices}), // data can be `string` or {object}!
         headers: new Headers({
             'Content-Type': 'application/json'
         })
