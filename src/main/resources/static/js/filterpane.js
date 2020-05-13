6aa3e0dc39b5f5a26b84e93d2cce562f4a0978ef
Wenhan Wang <wqy19970911@hotmail.com>
2020-05-12 22:22:22 -0400

    Fix filter interactives

--- src/main/resources/static/js/filterpane.js
+++ src/main/resources/static/js/filterpane.js
@@ -6,7 +6,9 @@ let filterpane = document.getElementById("filter-page")
 let filterclosebtn = document.getElementById("close-filter-button")
 
 filterclosebtn.onclick = function(event) {
+    filterpane.style.display = "none"
 }
 
 filter.onclick = function (event) {
+    filterpane.style.display = "block"
 }
