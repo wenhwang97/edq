8e17d48461f0b16fc80845d4bbc63fe15c6a7def
Wenhan Wang <wqy19970911@hotmail.com>
2020-05-12 22:18:04 -0400

    Fix filter interactives

---
+++ src/main/resources/static/js/filterpane.js
@@ -0,0 +1,12 @@
+let filter = document.getElementById("map-filter-button")
+let errs = document.getElementById("errors-list-button")
+let logs = document.getElementById("logs-button")
+
+let filterpane = document.getElementById("filter-page")
+let filterclosebtn = document.getElementById("close-filter-button")
+
+filterclosebtn.onclick = function(event) {
+}
+
+filter.onclick = function (event) {
+}
