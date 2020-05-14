960db80a03fc671225cd0d0025278d01444c4161
Wenhan Wang <wqy19970911@hotmail.com>
2020-05-14 15:21:52 -0400

    Add errors & logs pane

--- src/main/resources/static/js/fecthError.js
+++
@@ -1,7 +0,0 @@
-var clickedState;
-errs.onclick = async function (event) {
-    console.log("click on error fetch");
-    let response = await fetch("http://localhost:8080/state/" + clickedState + "/data/errors");
-    let errorJson = await response.json();
-    console.log(errorJson);
-}
--- src/main/resources/static/js/filterpane.js
+++ src/main/resources/static/js/filterpane.js
@@ -1,3 +1,10 @@
+let sidepaneData = document.getElementById("sidepane-data")
+let sidepaneErrLog = document.getElementById("sidepane-errlog")
+let errTab = document.getElementById("err-tab")
+let errContent = document.getElementById("tab3")
+let logTab = document.getElementById("log-tab")
+let logContent = document.getElementById("tab4")
+
 let filter = document.getElementById("map-filter-button")
 let errs = document.getElementById("errors-list-button")
 let logs = document.getElementById("logs-button")
@@ -5,12 +12,42 @@ let logs = document.getElementById("logs-button")
 let filterpane = document.getElementById("filter-page")
 let filterclosebtn = document.getElementById("close-filter-button")
 
-filterclosebtn.onclick = function(event) {
+// Display of filter window
+filterclosebtn.onclick = function() {
     filterpane.style.display = "none"
     filter.style.opacity = "1"
 }
 
-filter.onclick = function (event) {
+filter.onclick = function () {
     filterpane.style.display = "block"
     filter.style.opacity = ".5"
+}
+
+// Display of sidepane sidebar
+errs.onmousedown = function () {
+    errs.style.opacity = ".5";
+}
+
+errs.onmouseup = function () {
+    errs.style.opacity = "1";
+    sidepaneData.style.display = "none"
+    sidepaneErrLog.style.display = "block"
+    logTab.classList.remove("active")
+    logContent.classList.remove("active")
+    errTab.classList.add("active")
+    errContent.classList.add("active")
+}
+
+logs.onmousedown = function () {
+    logs.style.opacity = ".5"
+}
+
+logs.onmouseup = function () {
+    logs.style.opacity = "1"
+    sidepaneData.style.display = "none"
+    sidepaneErrLog.style.display = "block"
+    errTab.classList.remove("active")
+    errContent.classList.remove("active")
+    logTab.classList.add("active")
+    logContent.classList.add("active")
 }
