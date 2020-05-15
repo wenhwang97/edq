99f5939eb5df75b1ebaf427be2030a8d3130c057
cosecant <38693290+cosecant123@users.noreply.github.com>
2020-05-15 14:57:51 -0400

    add id

---
+++ src/main/resources/static/js/changeData.js
@@ -0,0 +1,5 @@
+var changeVoting = document.getElementById("SaveVoteChanges");
+changeVoting.onclick(function(){
+    console.log("change voting");
+    var rpVoting=document.getElementById("RepublicanVoting").
+})
