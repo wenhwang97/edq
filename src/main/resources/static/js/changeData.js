34e25c7e50d040286851a7d595d42c66d24b1958
cosecant <38693290+cosecant123@users.noreply.github.com>
2020-05-15 15:44:53 -0400

    add ids and some js

--- src/main/resources/static/js/changeData.js
+++ src/main/resources/static/js/changeData.js
@@ -1,5 +1,47 @@
 var changeVoting = document.getElementById("SaveVoteChanges");
-changeVoting.onclick(function(){
+var datatype = document.getElementById("Datatype");
+// changeVoting.onclick(function(){
+//     console.log("change voting");
+//     var dmVoting=document.getElementById("dmChanges").valueOf();
+//     console.log(dmVoting);
+//
+// })
+changeVoting.addEventListener('click', function(){
     console.log("change voting");
-    var rpVoting=document.getElementById("RepublicanVoting").
+    var dmVoting=document.getElementById("dmChanges").textContent;
+    var rpVoting=document.getElementById("rpChanges").textContent;
+    var lbVoting=document.getElementById("lbChanges").textContent;
+    var grVoting=document.getElementById("grChanges").textContent;
+
+    var origionrp = document.getElementById("RepublicanVoting").textContent;
+    var origiongr = document.getElementById("GreenVoting").textContent;
+    var origionlb = document.getElementById("LibertarianVoting").textContent;
+    var origiondm = document.getElementById("DemocraticVoting").textContent;
+    let RData;
+    let DData;
+    let LData;
+    let GData;
+    if(dmVoting==""){
+        console.log("it is null");
+        DData= parseInt(origiondm);
+    }else{
+        DData=parseInt(dmVoting);
+    }
+    if(rpVoting==""){
+        RData=parseInt(origionrp);
+    }else{
+        RData=parseInt(rpVoting);
+    }
+    if(lbVoting==""){
+        LData= parseInt(origionlb);
+    }else{
+        LData= parseInt(lbVoting);
+    }
+    if(grVoting==""){
+        GData = parseInt(origiongr);
+    }else{
+        GData = grVoting;
+    }
+    // var votingType=
+
 })
