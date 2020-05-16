019393428bbfe2844cbd7dfdddd299f51eac2a8b
cosecant <38693290+cosecant123@users.noreply.github.com>
2020-05-15 21:39:41 -0400

    modify error list

---
+++ src/main/resources/static/js/fecthError.js
@@ -0,0 +1,26 @@
+var clickedState;
+var VoteDemoErrorTable=  document.getElementById("VoteDemoErrorTable");
+var noVotingTable=  document.getElementById("NotVoteErrorTable");
+var noDemoTable=  document.getElementById("NotDemoErrorTable");
+var mutipolyTable=  document.getElementById("MutipolyTable");
+var overlapTable=  document.getElementById("OverLapTable");
+var gapTable=  document.getElementById("GapTable");
+var encloseTable=  document.getElementById("EncloseTable");
+var selfTable=  document.getElementById("SELFITable");
+// VoteDemoError
+// NoVoteError
+// NoDemoError
+var VoteDemoErrorNumber=  document.getElementById("VoteDemoError");
+var noVoteNumber=  document.getElementById("NoVoteError");
+var noDemoNumber=  document.getElementById("NoDemoError");
+var mutipolyNumber=  document.getElementById("MultipolyError");
+var overlapNumber=  document.getElementById("OverLapError");
+var gapNumber=  document.getElementById("GapError");
+var encloseNumber=  document.getElementById("EncloseError");
+var selfNumber=  document.getElementById("SELFIError");
+
+// errs.onclick = async function (event) {
+//     let response = await fetch("http://localhost:8080/state/" + clickedState + "/data/errors");
+//     let errorJson = await response.json();
+//     console.log(errorJson);
+// }
--- src/main/resources/static/js/filterpane.js
+++ src/main/resources/static/js/filterpane.js
@@ -10,7 +10,7 @@ let logContent = document.getElementById("tab4")
 let filter = document.getElementById("map-filter-button")
 let errs = document.getElementById("errors-list-button")
 let logs = document.getElementById("logs-button")
-
+var clickedState;
 let filterpane = document.getElementById("filter-page")
 let filterclosebtn = document.getElementById("close-filter-button")
 
@@ -30,12 +30,101 @@ errs.onmousedown = function () {
     errs.style.opacity = ".5";
 }
 
-errs.onmouseup = function () {
+errs.onmouseup = async function () {
     if (isNotToggled) {
         sidepane.classList.toggle("toggled")
         isNotToggled = false
-    }
+        console.log("open error");
+        if(clickedState!=null) {
+            let response = await fetch("http://localhost:8080/state/" + clickedState + "/data/errors");
+            let errorJson = await response.json();
+            console.log(errorJson);
+            var VOTEDEMOnum = 0;
+            var NOVOTEnum=0;
+            var NODEMOnum=0;
+            var Overlapnum=0;
+            var Gapnum=0;
+            var slefnum=0;
+            var enclosenum=0;
+            var multinum=0;
+            for(var i in errorJson){
+                if(errorJson[i].type=="VOTEDEMO"){
+                    VOTEDEMOnum++;
+                    var VotedemoErrobutton = document.createElement("button");
+                    VotedemoErrobutton.setAttribute("class","btn btn-link");
+                    VotedemoErrobutton.textContent=errorJson[i].info;
+                    VoteDemoErrorTable.appendChild(VotedemoErrobutton);
+                    VoteDemoErrorTable.appendChild(document.createElement("br"));
+                }
+                if(errorJson[i].type=="NOVOTE"){
+                    NOVOTEnum++;
+                    var NovoteErrobutton = document.createElement("button");
+                    NovoteErrobutton.setAttribute("class","btn btn-link");
+                    NovoteErrobutton.textContent=errorJson[i].info;
+                    noVotingTable.appendChild(NovoteErrobutton);
+                    noVotingTable.appendChild(document.createElement("br"));
+                }
+                if(errorJson[i].type=="NODEMO"){
+                    NODEMOnum++;
+                    var NoDemoErrobutton = document.createElement("button");
+                    NoDemoErrobutton.setAttribute("class","btn btn-link");
+                    NoDemoErrobutton.textContent=errorJson[i].info;
+                    noDemoTable.appendChild(NoDemoErrobutton);
+                    noDemoTable.appendChild(document.createElement("br"));
+                }
+                if(errorJson[i].type=="OVERLAP"){
+                    Overlapnum++;
+                    var Overlapbutton = document.createElement("button");
+                    Overlapbutton.setAttribute("class","btn btn-link");
+                    Overlapbutton.textContent=errorJson[i].info;
+                    overlapTable.appendChild(Overlapbutton);
+                    overlapTable.appendChild(document.createElement("br"));
+                }
+                if(errorJson[i].type=="GAP"){
+                    Gapnum++;
+                    var Gapbutton = document.createElement("button");
+                    Gapbutton.setAttribute("class","btn btn-link");
+                    Gapbutton.textContent=errorJson[i].info;
+                    gapTable.appendChild(Gapbutton);
+                    gapTable.appendChild(document.createElement("br"));
+                }
+                if(errorJson[i].type=="SELFINTER"){
+                    slefnum++;
+                    var Slefbutton = document.createElement("button");
+                    Slefbutton.setAttribute("class","btn btn-link");
+                    Slefbutton.textContent=errorJson[i].info;
+                    selfTable.appendChild(Slefbutton);
+                    selfTable.appendChild(document.createElement("br"));
+                }
+                if(errorJson[i].type=="ENCLOSED"){
+                    enclosenum++;
+                    var Enclosebutton = document.createElement("button");
+                    Enclosebutton.setAttribute("class","btn btn-link");
+                    Enclosebutton.textContent=errorJson[i].info;
+                    encloseTable.appendChild(Enclosebutton);
+                    encloseTable.appendChild(document.createElement("br"));
+                }
+                if(errorJson[i].type=="MULTIPOLY"){
+                    multinum++;
+                    var Multibutton = document.createElement("button");
+                    Multibutton.setAttribute("class","btn btn-link");
+                    Multibutton.textContent=errorJson[i].info;
+                    mutipolyTable.appendChild(Multibutton);
+                    mutipolyTable.appendChild(document.createElement("br"));
+                }
+            }
+            VoteDemoErrorNumber.textContent=VOTEDEMOnum;
+            noVoteNumber.textContent=NOVOTEnum;
+            noDemoNumber.textContent = NODEMOnum;
+            overlapNumber.textContent=Overlapnum;
+            gapNumber.textContent=Gapnum;
+            selfNumber.textContent=slefnum;
+            encloseNumber.textContent=enclosenum;
+            mutipolyNumber.textContent=multinum;
+        }
 
+    }
+    // console.log("open error");
     errs.style.opacity = "1";
     sidepaneData.style.display = "none"
     sidepaneErrLog.style.display = "block"
